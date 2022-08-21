5b9c4e2bd2a002e2f5100f715eb614b1bf128c7d
James Njenga <knjenga94@gmail.com>
2022-08-21 13:22:19 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+//has a function expression called divide
+//divide divides 2000 by 100
+
+const divide=function(){
+        return (2000/100);
+    }
+
+
+   // has an arrow function called square
+   // square arrow function takes one parameter and multiplies it times itself
+
+
+const square=num=>num**2;
+
+//has an arrow function called add
+   // add arrow function takes two parameters and adds them together
+
+const add=(num1,num2)=>num1+num2;
